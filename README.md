109761cd8289b78f46d8d542299fa9d7ca91d693
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-05-20 18:29:41 -0400

    Add pgp and 404

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # xyz
+
 Personal Website
+
+## Inspiration
+
+- https://github.com/tokio-rs/axum/tree/main/examples/static-file-server
+- https://github.com/tomhschmidt/PersonalWebsite
+- https://github.com/artursapek/isometric-cubes/blob/main/artcx/src/main.rs
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,48 @@
+use std::net::SocketAddr;
 use axum::{
     handler::HandlerWithoutStateExt, http::StatusCode, routing::{get, post}, Json, Router
 };
-use tower_http::services::{ServeDir, ServeFile};
+use tower_http::{
+    services::{ServeDir, ServeFile},
+    trace::TraceLayer,
+};
 
 use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+    // TODO:
     let not_found_service = handle_404.into_service();
 
     let app = Router::new()
-        .nest_service("/", get(root))
+        .nest_service("/", root())
+        .nest_service("/assets", ServeDir::new("assets"))
+        // TODO: move into static dir
+        // .nest_service("/akan72.pub", ServeFile::new("./assets/akan72.pub"))
+        .nest_service("/pgp", ServeFile::new("./assets/pgp.html"))
+        // TODO: combine into root service and return 404? or just generate page?
         .nest_service(
             "/static",
-            ServeDir::new("./static").not_found_service(not_found_service)
-            // ServeDir::new("./static").not_found_service(ServeFile::new("assets/cig.jpeg"))
+            // ServeDir::new("./static").not_found_service(not_found_service)
+            ServeDir::new("./static").not_found_service(ServeFile::new("./assets/index.html"))
         );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+fn root() -> Router {
+    async fn static_string_wrapper() -> (StatusCode, &'static str) {
+        (StatusCode::OK, "Hello, World!")
+    }
+
+    let service = static_string_wrapper.into_service();
+
+    Router::new()
+        // .route("/", get(|| async { "Hi from /foo" }))
+        .route("/foo", get( || async { "Hi from /foo" } ))
+        .fallback_service(service)
 }
 
 async fn handle_404() -> (StatusCode, &'static str) {
