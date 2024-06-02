c9620d4ce8a65d44ff5a46324b7d65ea88dac492
Alex Kan <29241719+akan72@users.noreply.github.com>
2024-06-02 11:01:44 -0400

    Update Axum routes and add html files

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,25 @@
-use std::net::SocketAddr;
-use axum::{
-    handler::HandlerWithoutStateExt, http::StatusCode, routing::{get, post}, Json, Router
-};
-use tower_http::{
-    services::{ServeDir, ServeFile},
-    trace::TraceLayer,
-};
-
-use serde::{Deserialize, Serialize};
+use axum::Router;
+use tower_serve_static::{ServeDir, ServeFile, include_file};
+use include_dir::{Dir, include_dir};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    // TODO:
-    let not_found_service = handle_404.into_service();
+
+    static ASSETS_DIR: Dir<'static> = include_dir!("$CARGO_MANIFEST_DIR/assets");
 
     let app = Router::new()
         .nest_service("/", root())
-        .nest_service("/assets", ServeDir::new("assets"))
-        // TODO: move into static dir
-        // .nest_service("/akan72.pub", ServeFile::new("./assets/akan72.pub"))
-        .nest_service("/pgp", ServeFile::new("./assets/pgp.html"))
-        // TODO: combine into root service and return 404? or just generate page?
-        .nest_service(
-            "/static",
-            // ServeDir::new("./static").not_found_service(not_found_service)
-            ServeDir::new("./static").not_found_service(ServeFile::new("./assets/index.html"))
-        );
+        .nest_service("/assets", ServeDir::new(&ASSETS_DIR))
+        .nest_service("/pgp", ServeFile::new(include_file!("/assets/pgp.html")))
+        .nest_service("/ideology", ServeFile::new(include_file!("/assets/ideology.html")));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 fn root() -> Router {
-    async fn static_string_wrapper() -> (StatusCode, &'static str) {
-        (StatusCode::OK, "Hello, World!")
-    }
-
-    let service = static_string_wrapper.into_service();
-
     Router::new()
-        // .route("/", get(|| async { "Hi from /foo" }))
-        .route("/foo", get( || async { "Hi from /foo" } ))
-        .fallback_service(service)
-}
-
-async fn handle_404() -> (StatusCode, &'static str) {
-    (StatusCode::NOT_FOUND, "Not Found")
+        .route_service("/", ServeFile::new(include_file!("/assets/index.html")))
+        .fallback_service(ServeFile::new(include_file!("/assets/404.html")))
 }
